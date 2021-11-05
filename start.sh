8d9d88b4d34f9c059b0b82c830a3a06c076f7485
Joel Matias Duran <55359469+joelmatiasduran@users.noreply.github.com>
2021-11-04 22:45:45 -0400

    Adding packages to the script

--- start.sh
+++ start.sh
@@ -4,11 +4,15 @@ docker-compose -f to-do-compose.yml up -d
 
 #This will start the Back-End "Nest Js"
 
-gnome-terminal -- sh -c "cd to-do-api && yarn && &&PORT=3006 yarn start:dev ; bash"
+gnome-terminal -- sh -c "cd to-do-api && yarn && PORT=3006 yarn start:dev ; bash"
+
+
 
 #This will start the Front-End
 
 cd to-do-frontend && yarn && yarn start
 
 #create a database with commands 
+
+
 
